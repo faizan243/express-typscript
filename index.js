cd3c3ade63a7545501c66dc153a23cf74630524c
faizankhandev <faizan480khan@gmail.com>
2024-04-26 09:59:48 -0700

    FIXED Import issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+    res.send("Express + TypeScript Server Faizan Khan");
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
